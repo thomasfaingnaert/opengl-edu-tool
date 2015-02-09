2f04021fe58ead7e4275e91f78080e6107500e33
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2015-02-09 19:51:20 +0100

    Added debug info and promoted openglwidget
    * Added debug info such as gl version, glsl version, renderer and vendor
    * Promoted the OpenGLWidget in mainwindow to our SceneWidget class

--- src/scenewidget.cpp
+++ src/scenewidget.cpp
@@ -1,5 +1,6 @@
 #include "scenewidget.h"
 #include <QSurfaceFormat>
+#include <iostream>
 
 SceneWidget::SceneWidget(QWidget *parent) :
     QOpenGLWidget(parent)
@@ -25,11 +26,16 @@ void SceneWidget::initializeGL()
 {
     // Init opengl
     initializeOpenGLFunctions();
+
+    // Print version info
+    std::clog << "OpenGL version: " << glGetString(GL_VERSION) <<
+                 "\nGLSL version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) <<
+                 "\nRenderer: " << glGetString(GL_RENDERER) <<
+                 "\nVendor: " << glGetString(GL_VENDOR) << '\n' << std::endl;
 }
 
 void SceneWidget::paintGL()
 {
-
 }
 
 void SceneWidget::resizeGL(int w, int h)
