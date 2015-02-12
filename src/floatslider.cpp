1b9b12290b2384c61485956a1bab367470dd2aa9
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2015-02-12 21:11:50 +0100

    Implemented FloatSlider
    * Added getters, setters and member variables for scale, precision, scale
    value and string representation
    * Added signals for scaledValueChanged and stringValueChanged

--- src/floatslider.cpp
+++ src/floatslider.cpp
@@ -1,9 +1,10 @@
 #include "floatslider.h"
+#include <QLocale>
 
 FloatSlider::FloatSlider(QWidget *parent) :
     QSlider(parent)
 {
-
+    connect(this, &QSlider::valueChanged, this, &FloatSlider::onValueChanged);
 }
 
 FloatSlider::~FloatSlider()
@@ -11,3 +12,14 @@ FloatSlider::~FloatSlider()
 
 }
 
+void FloatSlider::onValueChanged(int)
+{
+    emit scaledValueChanged(scaledValue());
+    emit stringValueChanged(getStringRep());
+}
+
+QString FloatSlider::getStringRep() const
+{
+    QLocale sysLocale = QLocale::system();
+    return sysLocale.toString(scaledValue(), 'f', m_precision);
+}
--- src/floatslider.h
+++ src/floatslider.h
@@ -2,12 +2,35 @@
 #define FLOATSLIDER_H
 
 #include <QSlider>
+#include <QString>
 
 class FloatSlider : public QSlider
 {
+    Q_OBJECT
+
 public:
-    explicit FloatSlider(QWidget *parent);
+    explicit FloatSlider(QWidget *parent = 0);
     ~FloatSlider();
+
+    float scale() const { return m_scale; }
+    void setScale(float scale) { m_scale = scale; }
+
+    int precision() const { return m_precision; }
+    void setPrecision(int precision) { m_precision = precision; }
+
+    float scaledValue() const { return value() * scale(); }
+    QString getStringRep() const;
+
+signals:
+    void scaledValueChanged(float scaledValue);
+    void stringValueChanged(QString strVal);
+
+private slots:
+    void onValueChanged(int value);
+
+private:
+    float m_scale = 0.01;
+    int m_precision = 2;
 };
 
 #endif // FLOATSLIDER_H
